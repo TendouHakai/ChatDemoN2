e15e7cdc4396e20e391e0420a41dc4d95dd34619
TendouHakai <thanhconghoahai@gmail.com>
2023-05-23 22:08:12 +0700

    update login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -26,7 +26,7 @@ const Login = () => {
   return (
     <div className="formContainer">
       <div className="formWrapper">
-        <span className="logo">Lama Chat</span>
+        <span className="logo">Demo Chat N2</span>
         <span className="title">Login</span>
         <form onSubmit={handleSubmit}>
           <input type="email" placeholder="email" />
